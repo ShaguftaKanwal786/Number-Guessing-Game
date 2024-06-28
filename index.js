b289b76bc7d3c4c20abe45bf9a17a7d6e72ae12e
ShaguftaKanwal786 <161942642+ShaguftaKanwal786@users.noreply.github.com>
2024-06-28 10:35:35 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+console.log(" \twelcome to NUmber Guessing Game\t");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+let answer = await inquirer.prompt([{
+        name: "guessnumber",
+        type: "number",
+        message: "plz select a number from 1 to 5 : ",
+    }]);
+if (answer.guessnumber === randomNumber) {
+    console.log("Congratulation you guess a correct number");
+}
+else {
+    console.log("Sorry ! you guess a wrong gnumber");
+}
+;
